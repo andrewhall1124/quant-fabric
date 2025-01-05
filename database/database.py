775690489d4ef7b696614b0ccd345b61a36ad33d
Andrew Hall <andrewmartinhall2@gmail.com>
2025-01-05 12:40:56 -0700

    Basic database

--- database/database.py
+++ database/database.py
@@ -1,9 +1,19 @@
+import polars as pl
+import os
+
+
 class Database:
     def __init__(self):
-        print("Database initialized")
+        self._tables_dir = "database/.tables/"
 
-    def connect(self):
-        print("Connecting to the database...")
+    def create(self, table_name: str, data: pl.DataFrame) -> None:
+        table_path = os.path.join(self._tables_dir, f"{table_name}.parquet")
+        data.write_parquet(table_path)
 
-    def close(self):
-        print("Closing the database connection...")
+    def read(self, table_name: str) -> pl.DataFrame:
+        table_path = os.path.join(self._tables_dir, f"{table_name}.parquet")
+        return pl.read_parquet(table_path)
+
+    def delete(self, table_name: str) -> None:
+        table_path = os.path.join(self._tables_dir, f"{table_name}.parquet")
+        os.remove(table_path)
---
+++ database/tests/crud_test.py
@@ -0,0 +1,29 @@
+from database import Database
+import polars as pl
+from polars.testing import assert_frame_equal
+import os
+
+data = pl.DataFrame(
+    [
+        {"a": 1, "b": 2, "c": 3},
+        {"a": 2, "b": 3, "c": 4},
+        {"a": 3, "b": 4, "c": 5},
+    ]
+)
+
+db = Database()
+
+
+def test_create():
+    db.create("test", data)
+    assert os.path.exists("database/.tables/test.parquet")
+
+
+def test_read():
+    test_data = db.read("test")
+    assert_frame_equal(test_data, data)
+
+
+def test_delete():
+    db.delete("test")
+    assert not os.path.exists("database/.tables/test.parquet")
