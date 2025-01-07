16ba1617dade4c17cceb57eef0edb4e854e74072
Andrew Hall <andrewmartinhall2@gmail.com>
2025-01-06 22:40:29 -0700

    Deleted old files

--- src/main.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-from src.datasets.toy_dataset import ToyDataset
-from qcomponents import ChunkedData
-from signals import momentum_signal
-from qdatabase import Database
-
-if __name__ == "__main__":
-    db = Database()
