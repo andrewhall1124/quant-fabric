625d9b1c499beadc9472462ec35fd5d7f4f15fda
Andrew Hall <andrewmartinhall2@gmail.com>
2025-01-06 20:51:24 -0700

    Working alpaca monthly data downloader.

--- qdatabase/database.py
+++ qdatabase/database.py
@@ -1,19 +1,56 @@
 import polars as pl
 import os
-
+import shutil
 
 class Database:
     def __init__(self):
-        self._tables_dir = "database/.tables/"
+        self._tables_dir = "qdatabase/.tables/"
+        self._archive_dir = "qdatabase/.archive/"
 
-    def create(self, table_name: str, data: pl.DataFrame) -> None:
-        table_path = os.path.join(self._tables_dir, f"{table_name}.parquet")
-        data.write_parquet(table_path)
+        os.makedirs(self._tables_dir, exist_ok=True)
+        os.makedirs(self._archive_dir, exist_ok=True)
+
+    def create(self, table_name: str, data: pl.DataFrame, overwrite: bool = False) -> None:
+        """
+        Method for creating a table in the database. If no data is given, an empty table is created.
+        All tables are stored locally as parquet files using polars.
+        """
+        table_path = self.get_table_path(table_name)
+
+        # Overwrite check
+        if self.exists(table_name) and not overwrite:
+            print("Unable to create table: already exists")
+            return
+
+        # Write non-empty or schema-defined DataFrame   
+        if data is not None or data.schema:  
+            data.write_parquet(table_path)
+
+        # Write empty dataframe
+        else:
+            pl.DataFrame().write_parquet(table_path)
 
     def read(self, table_name: str) -> pl.DataFrame:
-        table_path = os.path.join(self._tables_dir, f"{table_name}.parquet")
+        table_path = self.get_table_path(table_name)
         return pl.read_parquet(table_path)
+    
+    def insert(self, table_name: str, rows: pl.DataFrame) -> None:
+        table = self.read(table_name)
+        table = pl.concat([table, rows])
+        self.create(table_name, table, overwrite=True)
+
+    def archive(self, table_name: str) -> None:
+        src_table_path = self.get_table_path(table_name)
+        dst_table_path = os.path.join(self._archive_dir, f"{table_name}.parquet")
+        shutil.move(src_table_path, dst_table_path)
 
     def delete(self, table_name: str) -> None:
-        table_path = os.path.join(self._tables_dir, f"{table_name}.parquet")
+        table_path = self.get_table_path(table_name)
         os.remove(table_path)
+
+    def get_table_path(self, table_name: str) -> str:
+        return os.path.join(self._tables_dir, f"{table_name}.parquet")
+    
+    def exists(self, table_name: str) -> bool:
+        table_path = self.get_table_path(table_name)
+        return os.path.exists(table_path)
--- src/datasets/alpaca_stock.py
+++ src/datasets/alpaca_stock.py
@@ -5,22 +5,26 @@ from alpaca.data.enums import Adjustment, DataFeed
 from alpaca.data.models.bars import BarSet
 from alpaca.trading import TradingClient
 from alpaca.trading import GetAssetsRequest
-from alpaca.trading.enums import AssetClass, AssetStatus, AssetExchange
+from alpaca.trading.enums import AssetClass, AssetStatus
 from alpaca.trading.models import Asset
-from datetime import datetime
+from datetime import date
 import os
 from dotenv import load_dotenv
 import polars as pl
 from qdatabase import Database
 
-class AlpacaStock():
+class AlpacaStockMonthly():
 
-    def __init__(self, start_date: datetime, end_date: datetime | None = None) -> None:
+    def __init__(self, start_date: date, end_date: date | None = date.today()) -> None:
         self.start_date = start_date
         self.end_date = end_date
 
+        start = start_date.strftime('%Y-%m-%d')
+        end = end_date.strftime('%Y-%m-%d')
+        self.table_name = f"ALPACA_STOCK_MONTHLY_{start}_{end}"
+
         if not end_date:
-            end_date = datetime.today()
+            end_date = date.today()
 
         load_dotenv()
         api_key = os.getenv("ALPACA_API_KEY")
@@ -30,8 +34,9 @@ class AlpacaStock():
         self._trading_client = TradingClient(api_key, secret_key)
         self.db = Database()
 
-        # self._table_name
+    def download(self):
         self._download_and_stage()
+        self._transform()
         self._merge()
 
     def _download_and_stage(self):
@@ -41,22 +46,88 @@ class AlpacaStock():
         # Get stock bars request
         stock_bar_request = StockBarsRequest(
             symbol_or_symbols=symbols,
-            timeframe=TimeFrame(1, TimeFrameUnit.Day),
+            timeframe=TimeFrame(1, TimeFrameUnit.Month),
             start=self.start_date,
             end=self.end_date,
             adjustment=Adjustment.SPLIT,
             feed=DataFeed.IEX,
         )
+        print("Downloading Alpaca data")
         bar_set: BarSet = self._stock_client.get_stock_bars(stock_bar_request)
 
         # Parsing
-        df = bar_set.df.reset_index()
-        df = pl.from_pandas(df)
+        data = bar_set.df.reset_index()
+        stage_table = pl.from_pandas(data)
+
+        # Create stage table
+        print("Staging")
+        self.db.create(f"{self.table_name}_STG", stage_table)
+
+    def _transform(self):
+        print("Transforming")
+        xf_table = self.db.read(f"{self.table_name}_STG")
+
+        # Rename columns
+        xf_table = xf_table.rename({'symbol': 'ticker', 'timestamp': 'date'})
+
+        # Cast date type
+        xf_table = xf_table.with_columns(
+            pl.col('date').dt.date()
+        )
+
+        xf_table = xf_table.sort(by=["ticker", "date"])
+
+        self.db.create(f"{self.table_name}_XF", xf_table)
 
     def _merge(self):
-        pass
+        
+        # Create the core table if it doesn't already exist
+        core_table_name = 'ALPACA_STOCK_MONTHLY'
+        core_table_schema = {
+            "ticker": pl.Utf8,              
+            "date": pl.Date,
+            "open": pl.Float64,             
+            "high": pl.Float64,             
+            "low": pl.Float64,              
+            "close": pl.Float64,            
+            "volume": pl.Float64,           
+            "trade_count": pl.Float64,      
+            "vwap": pl.Float64,          
+        }
+        empty_core_table = pl.DataFrame(schema=core_table_schema)
+        self.db.create(
+            table_name=core_table_name, 
+            data=empty_core_table, 
+            overwrite=True
+        )
+
+        # Prepare tables for merge with source tracking
+        xf_table = self.db.read(f"{self.table_name}_XF").with_columns(pl.lit('xf').alias('source'))
+        core_table = self.db.read(core_table_name).with_columns(pl.lit('core').alias('source'))
+        
+        # Columns to compare on
+        unique_columns = core_table_schema.keys()
+
+        # Find unique rows
+        unique_rows = (
+            pl.concat([core_table, xf_table])
+            .unique(subset=unique_columns, keep='first')
+            .filter(pl.col('source') == 'xf')
+            .drop('source')
+        )
+
+        # Insert unique rows into core table
+        print(f"Inserting {len(unique_rows)} unique rows")
+        self.db.insert(core_table_name, unique_rows)
+
+        # Archive stage table
+        self.db.archive(f"{self.table_name}_STG")
+
+        # Delete xf table
+        self.db.delete(f"{self.table_name}_XF")
 
     def _get_symbols(self):
+        print("Getting available assets")
         # Alpaca get assets request
         search_params = GetAssetsRequest(
             status=AssetStatus.ACTIVE,
@@ -99,10 +170,16 @@ class AlpacaStock():
         return df['symbol']
 
 if __name__ == '__main__':
-    start = datetime(2025,1,1)
-    dataset = AlpacaStock(start_date=start)
+    years = range(2020,2025) # Unsure why years 2016-2019 don't have data...
+    for year in years:
+        print("-"*10 + f"{year}" + "-"*10)
+        start = date(year,1,1)
+        end = date(year, 12, 31)
+        dataset = AlpacaStockMonthly(
+            start_date=start,
+            end_date=end
+        ).download()
 
-# TODO: Set up dataset to download, stage, and merge the data for a specified time frame
 # TODO: Adapt the dataset to check which dates are already downloaded before running (make an option 'redownload')
 # TODO: Portfolio generator
 # TODO: Backtester module
