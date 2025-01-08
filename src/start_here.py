e9bbfb0c305255029f63ef2a259f8badfb8ba2d8
Andrew Hall <andrewmartinhall2@gmail.com>
2025-01-08 12:39:04 -0700

    Improved strategy output to be long short

--- src/start_here.py
+++ src/start_here.py
@@ -4,4 +4,4 @@ from strategies import momentum_strategy
 portfolios = momentum_strategy(type="monthly")
 
 # Print the 10 decile portfolios for the last period (December 2024 portfolios)
-print(portfolios[-1])
+print(portfolios)
--- src/strategies/momentum.py
+++ src/strategies/momentum.py
@@ -4,9 +4,10 @@ from src.datasets import AlpacaStockMonthly
 from src.optimizers import decile_portfolio
 from functools import partial
 from datetime import date
+import polars as pl
 
 
-def momentum_strategy(type: str = "daily"):
+def momentum_strategy(type: str = "daily") -> list[pl.DataFrame]:
     """
     This is the script for the classic momentum trading strategy.
     It should be able to be passed to both a backtester and a live/paper trader.
@@ -26,10 +27,19 @@ def momentum_strategy(type: str = "daily"):
     chunked_data.remove_chunks()
 
     # Generate portfolios
-    portfolios = chunked_data.apply_portfolio_gen(partial(decile_portfolio, signal='mom'))    
+    portfolios_list: list[list[pl.DataFrame]] = chunked_data.apply_portfolio_gen(partial(decile_portfolio, signal='mom')) 
 
-    return portfolios
+    # Long good momentum, short poor momentum
+    portfolios_list = [
+        pl.concat([
+            portfolios[0].with_columns(pl.col('weight') * -1),
+            portfolios[9]
+        ]).drop(['bin', 'mom'])
+        for portfolios in portfolios_list
+    ]
+
+    return portfolios_list
 
 if __name__ == "__main__":
-    portfolios = momentum_strategy("monthly")
-    print(portfolios[-1])
+    result = momentum_strategy("monthly")
+    print(result)
